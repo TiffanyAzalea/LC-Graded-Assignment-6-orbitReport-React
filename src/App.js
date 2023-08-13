053438040144f5a5b793e8ef9006fd8756ba83f3
Tiffany Hill <hill.tiffany365@gmail.com>
2023-08-13 17:41:35 -0400

    Updated Button.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import satData from "./components/satData";
 
 function App() {
   const [sat, setSat] = useState(satData);
-  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+  const displaySats = [...new Set(satData.map((satData) => satData.orbitType))];
 
   const filterByType = (currentType) => {
     const displaySats = satData.filter((newSatDisplay) => {
@@ -15,6 +15,8 @@ function App() {
     });
     setSat(displaySats);
  };
+
+//might need a key???
 
   return (
     <div>
