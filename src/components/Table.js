c9c3608baa0493b6d4b40931b6bdd659d193160f
Tiffany Hill <hill.tiffany365@gmail.com>
2023-08-13 22:29:28 -0400

    Added to table.js

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,21 +1,33 @@
+
+
 const Table = ({ sat }) => {
+  
+ 
   return (
       <table>
        <thead>
         <tr>
-          <th>Header TBD</th>
-          <th></th>
-          <th></th>
-          <th></th>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>Row Data TBD</td>
+         {displaySats.map((id, satData) => {
+          return 
+
+        <tr key={id}>
+          <td>{satData.name}</td>
+          <td>{satData.type}</td>
+          <td>{satData.launchDate}</td>
+          <td>{satData.operational ? "Active" : "Inactive"}</td>
         </tr>
+         })};
         </tbody>
       </table>
   );
 };
 
 export default Table;
+
